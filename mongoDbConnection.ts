1b26c7b56b2f4f05061ea237f1bd7d219cbe1efc
joel <joelthousend@gmail.com>
2024-06-23 06:41:03 +0300

    added proecss.env.MONGODB_URI

--- mongoDbConnection.ts
+++ mongoDbConnection.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const uri = "mongodb://127.0.0.1:27017/clubhouse?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6";
+const devUri="mongodb://127.0.0.1:27017/clubhouse?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6";
+const uri:string = process.env.MONGODB_URI||"";
 
 async function connectDB() {
   try {
